1fa12a25598ddd6c2fa0c4bd1b50778c0f3c974d
ookawa0602 <forefront.ookawa2@gmail.com>
2020-07-30 23:50:40 +0900

    index firstCommit

--- resources/views/index/indexlayout.blade.php
+++ resources/views/index/indexlayout.blade.php
@@ -4,9 +4,23 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/css/swiper.css">
 <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 
+<link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
 </head>
 <body>
+ <script src="https://code.jquery.com/jquery-1.11.3.js"></script>
+  <script src="https://unpkg.com/scrollreveal"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.js"></script>
+ <!--ヘッダー-->
+ <script type="text/javascript">
+$(function(){
+  ScrollReveal().reveal('.speed', { distance: '200px', origin: 'left', viewFactor: '0.8' });
+  ScrollReveal().reveal('.speed1', { duration: '1800' ,delay: '1000'} );
+  });
+ </script>
+ <header>
+  <p class="speed">完全無料の健康管理ツール</p>
+  <h2 class="speed1">楽エット</h2>
+ </header>
   <div class="swiper-container">
     <div class="swiper-wrapper">
       <div class="swiper-slide"><img class="logo" src="{{ asset('img/test.jpeg') }}" width="700" height="400">
