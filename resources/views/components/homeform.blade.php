37377c9826cf57a9ad0bda73f6d8278127ed4db5
ookawa0602 <forefront.ookawa2@gmail.com>
2020-08-14 00:18:22 +0900

    home css smartphone

---
+++ resources/views/components/homeform.blade.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="{{ asset('css/homeform.css') }}">
+<p class="homedai">{{ Auth::user()->name }}さんようこそラックエットへ</p>
+<!--     {{ Auth::user()->email }}-->
+<form action="home2" method="post">
+    {{ csrf_field() }}
+  <button class="start" type='submit' name='start' value='{{ Auth::user()->email }}'>体重管理シートを作成する</button>
+</form>
+<form action="home" method="post">
+   {{csrf_field() }}
+<button class="change" type='submit' name='change' value='{{ Auth::user()->email }}'>体重管理シートを変更する</button>
+</form>
+<!--
+<form action="home2" method="post">
+      {{ csrf_field() }}
+   <button class="dell" type='submit' name='dell' value='{{ Auth::user()->email }}'>体重管理シートを削除する</button>
+</form>-->
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="https://cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
 <link rel="stylesheet" href="https://cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
 <!--cssファイル-->
-<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+<!--<link rel="stylesheet" href="{{ asset('css/home.css') }}">-->
 <!--jsファイルを読み込む-->
 <script src="https://code.jquery.com/jquery-1.11.3.js"></script>
 <script src="https://unpkg.com/scrollreveal"></script>
@@ -20,23 +20,8 @@
                             {{ session('status') }}
                         </div>
 		    @endif
-		  
-		    <p>ようこそラックエットへ</p>
-                    <p>ようこそラックエットへ</p>
-                     <p>ようこそラックエットへ</p>
-		      <p>ようこそラックエットへ</p>
-		      <p>ここの中の枠組みの大きさは文章によって変化します</p>
-                      {{ Auth::user()->name }} 
-		      {{ Auth::user()->email }}
-		    {{ __('You are logged in!') }}
-                   <form action="home2" method="post">
-                     {{ csrf_field() }}
-		     <button class="start" type='submit' name='start' value='{{ Auth::user()->email }}'>体重管理シートを作成する</button>
-		  </form>
-		  <form action="home2" method="post">
-                    {{ csrf_field() }}
-                    <button class="dell" type='submit' name='dell' value='{{ Auth::user()->email }}'>体重管理シートを削除する</button>
-		 </form>
+		    @component('components.homeform')
+                    @endcomponent     
                 </div>
             </div>
         </div>
@@ -49,8 +34,11 @@
 <!--js-->
 <script type="text/javascript">
 $(function(){
+ if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+ }else{
 	ScrollReveal().reveal('.card', { distance: '200px', origin: 'right', viewFactor: '0.8' });
 	ScrollReveal().reveal('.menubar', { duration: 1600, origin: 'bottom', distance: '50px',reset: true });
+ }
 });
 </script>
 @endsection
