0cd4ddf9b36024e818019eeb4a0d592122b6dd92
ookawa0602 <forefront.ookawa2@gmail.com>
2020-08-16 16:33:42 +0900

    table delete css

--- resources/views/home/home2.blade.php
+++ resources/views/home/home2.blade.php
@@ -4,8 +4,7 @@
 <h1>ここの箇所にボディーデータなどを入力して登録する画面を作る</h1>
 <form action="home2" method="post">
  {{csrf_field() }}
-<button class="dell" type='submit' name="dell" value=''>会員IDを削除する
-</button>
+<button class="dell" type='submit' name="dell" value=''>会員データ削除画面へ</button>
 </form>
 @endsection
 
--- resources/views/home/homedell.blade.php
+++ resources/views/home/homedell.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.apphome')
 
 @section('body')
+<link rel="stylesheet" href="{{ asset('css/homedell.css') }}">
+<h3 class="delldai">CAUNTION!!!</h3>
+<p class="dai">【注意】</p>
+<p class="dell">会員情報を削除すると、会員情報や今までの体重データや
+ボディデータなどはすべて削除され、元に戻せなくなります。よろしいですか？</p>
 <form action="dellmember" method="post">
 {{csrf_field() }}
-<button class="dellmember" type='submit' name='dellmember' value="{{ Auth::user()->email }}">会員データを完全に削除する</button>
+<button class="dellmember" type='submit' name='dell' value="{{ Auth::user()->email }}">会員データを完全に削除する</button>
 @endsection
