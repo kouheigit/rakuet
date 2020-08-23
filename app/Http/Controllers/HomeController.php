b1eb00d3f4abbd5ca419fee59b8c8769f32cc03f
ookawa0602 <forefront.ookawa2@gmail.com>
2020-08-23 21:08:02 +0900

    ホーム画面・ボディ画面のシステム完了

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,9 +30,60 @@ class HomeController extends Controller
             $moji1 = str_replace('@','',$moji1);
             $moji2 = str_replace('.','',$moji1);
 	    $user = $moji2; 
-	    $age = DB::table($user)->select('age')->get();
-        return view('home',compact('age'));
-    }
+	    $atai = DB::table($user)->where('id','1')->first();
+	    $age = $atai->age;
+	    $sexual =$atai->sexual;
+	    $height =$atai->height;
+	    $weight =$atai->weight;
+	    
+	    //BMIの計算
+	    if(!is_null($height)&&!is_null($weight))
+	    {
+	       $height1 = $height/100;
+	       $height2 = $height1 * $height1;
+	       $kei = $weight / $height2;
+	       $BMI = round($kei);
+	    }else{
+		    $BMI = "未入力項目があります";
+	    }
+
+	    //基礎代謝の計算
+	    if(is_null($sexual)){
+		    $BMR = "未入力の項目があります";
+	     }else{
+	    if(($sexual=="男")&&(!is_null($age)&&!is_null($height)&&!is_null($weight))){
+		    $kei1 = 13.397 * $weight +4.799 * $height - 5.677 * $age + 88.362;
+                    $BMR = round($kei1);
+
+	    }elseif(($sexual=="女")&&(!is_null($age)&&!is_null($height)&&!is_null($weight)))
+	    {
+		    $kei1 =  9.247 * $weight + 3.098 *  $height - 4.33 * $age + 447.593;
+		    $BMR = round($kei1);
+	    }else{
+		    $BMR = "未入力の項目があります";
+	    }
+	    
+	    //肥満度の計算
+	    if(18 > $BMI){
+		    $himan ="低体重";
+	    }elseif($BMI >=18 && $BMI < 25){
+		    $himan ="普通体重";
+	    }elseif($BMI >=25  && $BMI < 30){
+		    $himan ="肥満度１";
+	    }elseif($BMI >= 30 && $BMI < 35){
+		    $himan ="肥満度２";
+	    }elseif($BMI >= 35 && $BMI < 40){
+		    $himan ="肥満度３";
+	    }elseif($BMI > 40){
+		    $himan ="肥満度４";
+	    }else{
+		    $himan="未入力の項目があります";
+	    }
+
+	    return view('home',compact('BMI','BMR','himan'));
+	    }	    
+	   } 
+	    
     public function homes(Request $request)
     {       
 	    $users = Auth::user()->email;
