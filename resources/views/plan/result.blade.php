fff95a8379d9a640717d02e9674cbc8b9f893a9f
ookawa0602 <forefront.ookawa2@gmail.com>
2020-09-23 21:02:35 +0900

    result画面装飾1

--- resources/views/plan/result.blade.php
+++ resources/views/plan/result.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.appplan')
 <!--CSSを読み込む-->
-<!--<link rel="stylesheet" href="{{ asset('css/homedell.css') }}">-->
+<link rel="stylesheet" href="{{ asset('css/result.css') }}">
 @section('body')
-
-{{$plandb}}
+<p class="sub">あなたにおすすめなダイエット方法は………</p>
+<h1 class="title">{{$plandb}}</h1>
 <br>
-{{$plandb1}}
+<p class="text">{{$plandb1}}</p>
 @endsection
 
