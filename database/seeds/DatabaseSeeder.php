c8d9d99bcbea1705d97f601816c0d55b65ac03f2
ookawa0602 <forefront.ookawa2@gmail.com>
2020-11-24 00:29:52 +0900

    gitテストまだ

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-	    $this->call(PlanTableSeeder::class);
+	  $this->call(PlanTableSeeder::class);
 	   // $this->call(usertableseeder::class);
         // $this->call(UserSeeder::class);
     }
