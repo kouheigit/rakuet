084076d7aac7a81e5d181298715e4b9e1b10ef3e
ookawa0602 <forefront.ookawa2@gmail.com>
2020-11-25 00:01:43 +0900

    seeder動作確認&casheirインストール苦戦

---
+++ database/migrations/2020_11_21_061127_create_testtable_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTesttableTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testtable', function (Blueprint $table) {
+            $table->id();
+            $table->string('title','30');
+            $table->string('text','1000');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testtable');
+    }
+}
