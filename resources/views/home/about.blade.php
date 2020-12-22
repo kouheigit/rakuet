21f72e529df4cfa72c7bfd06305a12b4a90d0454
ookawa0602 <forefront.ookawa2@gmail.com>
2020-12-22 09:55:47 +0900

    about画面作成202012.22

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.apphome')
+@extends('layouts.about')
 @section('body')
 <link rel="stylesheet" href="{{ asset('css/about.css') }}">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -7,6 +7,10 @@
 <h1 id="method1">手順１　ボディデータを登録する</h1>
 <p class="main1">まずはホーム画面のボディデータ登録画面でボディデータを登録しましょう下記のような状態ではまだ楽エットの機能は使用できません。まずはホーム画面のボディデータの登録、編集ボタンを押して下さい</p>
 <img class="step1" src="{{ asset('img/step1.png') }}">
-<p class="main2">登録ボタンを押すと次はボディデータを登録出来ます、身長、体重、年齢、性別を入力したらBMI、BMRをなどの計算して表示してくれます。</p>
+<p class="main2">登録ボタンを押すと次はボディデータを登録出来ます、身長、体重、年齢、性別を入力し終えたらBMI、BMR、あなたに適切な運動量などを計算して表示してくれます。</p>
+<img class="step2" src="{{ asset('img/step2.png') }}">
+<p class="main3">ボディデータ登録後ホーム画面が以下のような画面に切り替わります。↓</p>
+<img class="step2" src="{{ asset('img/step3.png') }}">
+<h1 id="method2">手順２　プラン診断を受ける</h1>
 @endsection 
 
