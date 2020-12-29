ab5760801aa8c8218ee46d9862ef331f0f48186e
ookawa0602 <forefront.ookawa2@gmail.com>
2020-12-29 22:00:05 +0900

    タグ整理

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,7 +1,8 @@
 @extends('index.indexlayout')
 
 @section('content')
-
+<title>楽エット|完全無料の健康管理ツール</title>
+<meta name="description" content="楽エットはあなたの体に負担の少ないダイエット方法を診断、ダイエットサポートをする健康管理ツールです。中々痩せない、痩せたいけどハードな運動や辛いダイエットが苦手な方におすすめのツールです"/>
 <form action="index2" method="post">
 {{ csrf_field() }}
   <button class="start" type='submit' name='start' value='start'>スタートする</button>
